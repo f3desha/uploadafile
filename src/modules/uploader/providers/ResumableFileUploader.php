ec9fe460b67e6a35ad3bb5a53701e9c45d57e157
Oleksandr Kuzmenko <f3dstudiya@gmail.com>
2024-02-14 01:49:51 +0000

    Cleaning

--- src/modules/uploader/providers/ResumableFileUploader.php
+++ src/modules/uploader/providers/ResumableFileUploader.php
@@ -4,6 +4,7 @@ class ResumableFileUploader
 {
     const UPLOAD_FOLDER_PATH = "../uploads/";
     const CHUNKS_FOLDER_PATH = "../chunks/";
+
     public function check($filename, $chunkNumber)
     {
         $chunkDir = $this->getChunkDirectory();
@@ -16,19 +17,15 @@ class ResumableFileUploader
         $targetDir = $this->getUploadDirectory();
         $chunkDir = $this->getChunkDirectory();
 
-        // Create directory for the chunks if it doesn't exist
         if (!file_exists($chunkDir)) {
             mkdir($chunkDir, 0777, true);
         }
 
-        // Move the uploaded chunk to the chunk directory
         move_uploaded_file($_FILES['file']['tmp_name'], $chunkDir . $filename . '.' . $chunkNumber);
 
-        // Check if all chunks have been uploaded
         $totalChunks = isset($resumableTotalChunks) ? (int)$resumableTotalChunks : 1;
 
         if ($chunkNumber == $totalChunks) {
-            // All chunks have been uploaded, combine them into the final file
             $finalPath = $targetDir . $filename;
             $finalFile = fopen($finalPath, 'wb');
 
@@ -37,13 +34,12 @@ class ResumableFileUploader
                 $chunkFile = fopen($chunkPath, 'rb');
                 stream_copy_to_stream($chunkFile, $finalFile);
                 fclose($chunkFile);
-                unlink($chunkPath); // Delete the chunk after it has been merged
+                unlink($chunkPath);
             }
 
             fclose($finalFile);
             echo "File uploaded successfully.";
         } else {
-            // Chunk uploaded successfully
             echo "Chunk $chunkNumber uploaded successfully.";
         }
 
